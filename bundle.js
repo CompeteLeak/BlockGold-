a99ee2c945411140064f740ebf64f1f1823b710c
maw <malikiw20@gmail.com>
2018-02-19 20:24:14 -0500

    Current PRogress, Methods Calls not Working

---
+++ bundle.js
@@ -0,0 +1,10 @@
+// var _ = require('lodash');
+// var $ = require('jquery'); 
+const SHA256 = require("crypto-js/sha256");
+
+// _([1,2,2,3,4,4,4,5,6,7,8,9,])
+//     .uniq()
+//         .each(function(i){
+//             console.log(i);
+//         }
+//     ); 
